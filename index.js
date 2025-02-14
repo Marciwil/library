045c7872489e5bb5c119faa77004f5b04b3bc0cb
Marcie Williams <190632913+Marciwil@users.noreply.github.com>
2025-02-13 19:01:10 -0500

    Created a 'read status' button to change book reading status form 'not read' to 'read'. Also created a button to remove abook from the book display.

--- index.js
+++ index.js
@@ -16,48 +16,55 @@
 // 5. Add a button on each display to remove the book from the library. Give them an attribute
 // for data that corresponds to the index of the library array.
 // 6. Add a button on each book's display to change its read status. Create a book prototype
-// that toogles a book'sinstance's read status.
+// that toogles a book's instance's read status.
 
-const myLibrary = [];
+let myLibrary = [];
 
 function Book(title, author, pages, read, genre) {
     this.title = title;
     this.author = author;
     this.pages = pages;
-    this.read = read;
+    this.readStatus = read;
     this.genre = genre;
     this.info = function() {
         console.log(this.title, this.author, this.pages, this.read, this.genre)
     };
 };
 
-// Adds the function to myLibrary array //
+// Change "read status" button //
+Book.prototype.changeToReadStatus = function(readCell) {
+    this.readStatus = "Read"
+    readCell.textContent = this.readStatus
+};
+
+// Remove a book fomr the display //
+Book.prototype.removeBook = function(rowToRemove) {
+    myLibrary = myLibrary.filter((bookToCheck) => {
+        this.title != bookToCheck.title
+    })
+    table.removeChild(rowToRemove)
+};
+
+// Adds the content to myLibrary array //
 function addBookToLibrary(title, author, pages, read, genre) {
     const book = new Book(title, author, pages, read, genre);
     myLibrary.push(book);
-}
+};
 
-//Book Array //
+//Book Array Examples //
 
-addBookToLibrary( "Twilight","Meyer, Stephanie","544 pages","Read","Romance, Supernatural");
+addBookToLibrary( "Twilight","Meyer, Stephanie","544 pages","Not Read","Romance, Supernatural");
 
-addBookToLibrary("Thigh High: Reiwa Hanamaru Academy Vol.1","Kotobuki","137 pages","Read", "Teen, Drama",);
+addBookToLibrary("Thigh High: Reiwa Hanamaru Academy Vol.1","Kotobuki","137 pages","Not Read", "Teen, Drama",);
 
-addBookToLibrary("Go for it! Nakamura!","Syundei","188 pages","Read","Teen, Roamance, Drama");
+addBookToLibrary("Go for it! Nakamura!","Syundei","188 pages","Not Read","Teen, Roamance, Drama");
 
-addBookToLibrary("Midnight Sun", "Meyer, Stephanie","662 pages","Mostly Read","Romance, Vampire, Fiction");
+addBookToLibrary("Midnight Sun", "Meyer, Stephanie","662 pages","Not Read","Romance, Vampire, Fiction");
 
-addBookToLibrary("Heaven's Official Blessing Book.1","Mo Xiang Tong Xiu","417 pages","Read","Romance, Fantasy, Fiction");
-
-
-
-
-
-//console.log('Books:', myLibrary)
+addBookToLibrary("Heaven's Official Blessing Book.1","Mo Xiang Tong Xiu","417 pages","Not Read","Romance, Fantasy, Fiction");
 
 // Events //
 
-// const table = document.querySelector("#library");
 let form = document.querySelector('form')
 let titleInput =document.getElementById('title')
 let authorInput = document.getElementById('author')
@@ -69,12 +76,12 @@ const container = document.querySelector('#container')
 const bookBtn = document.querySelector("#addBtn");
 const table = document.querySelector('#tableBody');
 
+
 // Display books onto table //
 function displayBooks() {
     table.childNodes.forEach((child) => {
         table.removeChild(child)
     })
-    
 
     myLibrary.forEach((book) => {
         const newRow = document.createElement('tr');
@@ -90,35 +97,51 @@ function displayBooks() {
         newRow.appendChild(authorCell);
 
         const pageCell = document.createElement('td');
-        pageCell.textContent = book.page
+        pageCell.textContent = book.pages
         newRow.appendChild(pageCell);
 
         const readCell = document.createElement('td');
-        readCell.textContent = book.read
+        readCell.classList.add('readCell')
+        readCell.textContent = book.readStatus
+
+        const readBtn = document.createElement('button')
+        readBtn.setAttribute('id','readBtn')
+        readBtn.textContent = "Read?";
+        readBtn.addEventListener( 'click', () => { 
+            console.log(book)
+            book.changeToReadStatus(readCell)
+        });
+        readCell.appendChild(readBtn);
         newRow.appendChild(readCell);
 
         const genreCell = document.createElement('td');
         genreCell.textContent = book.genre
         newRow.appendChild(genreCell);
 
+        const removeBookBtn = document.createElement('button');
+        removeBookBtn.textContent = "Remove"
+        newRow.appendChild(removeBookBtn);
+        removeBookBtn.addEventListener( 'click', () => {
+            book.removeBook(newRow)
+        });
+
         table.appendChild(newRow);
-    })
+    });
 };
 
 displayBooks()
 
+// button to add new books //
 bookBtn.addEventListener('click', () => {
+
     let bookTitle = titleInput.value
     let bookAuthor = authorInput.value
     let bookPage = pageInput.value
     let bookRead = readInput.value
     let bookGenre = genreInput.value
 
-        
-
-    addBookToLibrary(bookTitle, bookAuthor, bookPage, bookRead, bookGenre)
-
-    displayBooks()
+    addBookToLibrary(bookTitle, bookAuthor, bookPage, bookRead, bookGenre);
+    displayBooks();
      
     // Clears Table Value //
     titleInput.value =''
@@ -129,4 +152,8 @@ bookBtn.addEventListener('click', () => {
 
 
 });
+
+// Read Status Button //
+// when this button is clicked, I want the read status to be "Read"
+// else it should read "Not Read".
 
